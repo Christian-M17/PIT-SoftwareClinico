30fd2e445637cefcb5c96fe565b1b14cf8b7e2e1
Fernando <99261191+FnnABR@users.noreply.github.com>
2023-09-13 17:59:02 -0300

    Update Selecione.php
    Consertando os textos inuteis

--- Selecione.php
+++ Selecione.php
@@ -19,22 +19,22 @@
       <div class="profile-name">Teste</div>
     </div> -->
   </header>
-  <main>
+    <main>
     <div class="card">
       <img src="img/paciente-icon.png">
       <div class="card__content">
         <a href="calendario.php">
         <p class="card__title">Calendario</p>
-        <p class="card__description">A agenda permite aos médicos e funcionários agendar consultas para pacientes. Isso inclui especificar datas, horários, tipos de consultas e informações do paciente.</p>
+        
       </div>
       </a>
     </div>
     <div class="card">
       <img src="img/agenda-icon.png">
       <div class="card__content">
-         <a href="agenda.php">
+         <a href="agenda.html">
         <p class="card__title">Agenda</p>
-        <p class="card__description">o calendário permite agendar recursos, como salas de exames, equipamentos médicos e pessoal, garantindo uma alocação eficiente de recursos.</p>
+        
       </div>
       </a>
     </div>
@@ -43,11 +43,7 @@
       <div class="card__content">
          <a href="clientes.php">
         <p class="card__title">Clientes</p>
-        <p class="card__description">Informações do Paciente: Nomes, datas de nascimento, sexo, endereços, números de telefone e outras informações de contato.
-
-          Histórico Médico: Registros de visitas anteriores, diagnósticos, tratamentos e prescrições médicas.
-          
-          Agendamento: Detalhes sobre consultas futuras, horários disponíveis e histórico de agendamento.</p>
+        
       </div>
       </a>
     </div>
@@ -56,30 +52,7 @@
       <div class="card__content">
          <a href="usuarios.php">
         <p class="card__title">Usuarios</p>
-        <p class="card__description">Informações do Usuário: Nomes, cargos, informações de contato e credenciais de login.
-
-          Permissões de Acesso: Níveis de acesso e permissões atribuídos a cada usuário, definindo quais partes do sistema eles podem acessar e quais ações podem realizar.
-          
-          Histórico de Acesso: Registros de quando cada usuário acessou o sistema, quais ações realizaram e quais informações visualizaram ou modificaram.</p>
-      </div>
-      </a>
-    </div>
-    <div class="card">
-      <img src="img/usuario.png">
-      <div class="card__content">
-         <a href="itens.php">
-        <p class="card__title">Itens</p>
-        <p class="card__description">Informações dos Itens: Nomes, informações, credenciais.
-          Histórico de Itens: Registros de quando cada Item, quais ações realizaram e quais informações visualizaram ou modificaram.</p>
-      </div>
-      </a>
-    </div>
-    <div class="card">
-      <img src="img/usuario.png">
-      <div class="card__content">
-         <a href="">
-        <p class="card__title">Manuntençao</p>
-        <p class="card__description">rem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specim</p>
+        
       </div>
       </a>
     </div>
