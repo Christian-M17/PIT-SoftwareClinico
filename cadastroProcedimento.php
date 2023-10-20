3b526fa5c85c48330bb71d5d9a4ba1e80976db95
Fernando <99261191+FnnABR@users.noreply.github.com>
2023-10-20 15:06:30 -0300

    Update cadastroProcedimento.php
    Mudanças que o Christian pediu: junção, deletar botões e estilizar.

--- cadastroProcedimento.php
+++ cadastroProcedimento.php
@@ -50,32 +50,38 @@ if ($login == null) {
             <input class="text-wrapper" type="number" name="duracao" placeholder="duracao">
             <img class="codicon-input" src="img/Vector-6.png" />
           </div>
+          
         </div>
+
+        <label class="text2"> Uso de itens</label>
+        <div class="input">
+          <div class="placeholder">
+            <select class="text-wrapper" name="clinica">
+              <option value="0"> <!-- Escreve ai --></option>
+            </select>
+            <img class="codicon-input" src="img/Vector-4.png" />
+          </div>
+        </div>
+        <div class="input">
+          <div class="placeholder">
+            <input class="text-wrapper" type="number" name="quantidade" placeholder="quantidade">
+            <img class="codicon-input" src="img/Vector-5.png" />
+          </div>
+        </div>
+
         <button type="submit" name="confirmar" class="button2">Cadastrar</button>
         <button name="enviar" type="submit" class="button2">Cancelar</button>
-        <label> <a href="Logado.php" class="text3">Voltar ></a></label>
+        
       
-      <section class="right-content2">
-        <div class="input3">
-          <label class="text2">Adicionar Item</label>
-          <div class="input4">
-            <div class="placeholder">
-              <select class="text-wrapper" name="clinica">
-                <option value="0"> <!-- Escreve ai --></option>
-              </select>
-              <img class="codicon-input" src="img/Vector-4.png" />
-            </div>
-          </div>
-          <div class="input4">
-            <div class="placeholder">
-              <input class="text-wrapper" type="number" name="quantidade" placeholder="quantidade">
-              <img class="codicon-input" src="img/Vector-5.png" />
-            </div>
-          </div>
+  
+        
           
-          <button type="submit" name="confirmar" class="button2">Adicionar Item</button>
-        </div>
-      </section>
+
+          
+    
+        
+    
+      <label> <a href="Logado.php" class="text3">Voltar ></a></label>
       <?php
 
       if (isset($_POST['confirmar'])) {
